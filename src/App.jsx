969e98656742ea62e5513986a8ecbc7f0ecceb20
JayanthYadav123 <yjayanth60@gmail.com>
2024-07-22 21:29:07 +0530

    dvsvd

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,20 @@
-// import React from 'react';
-// import StudentCard from './components/StudentCard.jsx';
-// import './App.css';
-// import ReactLogo from './assets/eye.svg';
+import React from 'react';
+import StudentCard from './components/StudentCard.jsx';
+import './App.css';
+import ReactLogo from './assets/eye.svg';
 
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <StudentCard 
-//         name="G Venkata Pavan Kumar" 
-//         registrationNumber="212221240013"
-//         photo={ReactLogo}
-//       />
-//     </div>
-//   );
-// }
+function App() {
+  return (
+    <div className="App">
+      <StudentCard 
+        name="G Venkata Pavan Kumar" 
+        registrationNumber="212221240013"
+        photo={ReactLogo}
+      />
+    </div>
+  );
+}
 
-// export default App;
+export default App;
+
